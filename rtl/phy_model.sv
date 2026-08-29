320db817a3fa90f76dfba4db772384ebce3c7f29
Andrew Tsomik <andrew.tsomik@gmail.com>
2026-08-29 18:55:10 -0400

    Completed phy model

---
+++ rtl/phy_model.sv
@@ -0,0 +1,93 @@
+//This is only a simulation PHY as the FPGA I use doesn't have a built-in one
+
+module phy_model(
+	output logic rx_clk,
+	output logic [3:0] rx_dt,
+	output logic rx_dt_valid,
+	output logic rx_er	
+);
+	//25 MHz clock generator
+	initial rx_clk = 1'b0;
+	always #20 rx_clk = ~rx_clk;
+	
+	//States
+	typedef enum logic[2:0]{
+		IDLE,
+		PREAMBLE,
+		SFD,
+		FRAME
+	}byte_state_t;
+	
+	byte_state_t current_state;
+	logic [7:0] byte_idx;
+	logic [7:0] frame_data [0:103];
+	
+	//Set counter at the start
+	//Read from hex file for input
+	initial begin 
+		byte_idx = 0;
+		$readmemh("data.hex", frame_data);
+	end
+
+	always_ff @(posedge rx_clk) begin
+		//Cycle counter
+		if(byte_idx < 248) begin
+			byte_idx <= byte_idx + 1;
+		end else begin
+			byte_idx <= 0;
+		end
+	end
+
+	always_comb begin
+		//State transition logic
+		if(byte_idx < 24) begin
+                        current_state = IDLE;
+                end else if(byte_idx < 38) begin
+                        current_state = PREAMBLE;
+                end else if(byte_idx < 40) begin
+                        current_state = SFD;
+                end else begin
+                        current_state = FRAME;
+                end
+		
+		//State output logic
+		case(current_state)
+			IDLE : begin
+				rx_dt_valid = 0;
+				rx_dt = 0;
+			end
+			PREAMBLE : begin 
+				rx_dt_valid = 1;
+				rx_dt = 4'h5;
+			end
+			SFD : begin
+				rx_dt_valid = 1;
+				if(byte_idx[0]) begin
+			       		rx_dt = 4'hD;
+				end 
+				else begin
+					rx_dt = 4'h5;
+				end
+			end
+			FRAME : begin 
+				rx_dt_valid = 1;
+				if(byte_idx[0]) begin
+					//byte_idx[0] is high when the second
+					//part of the byte is being read and
+					//vice versa
+					rx_dt = frame_data[(byte_idx - 40)/2][7:4];
+				end else begin
+					rx_dt = frame_data[(byte_idx - 40)/2][3:0];
+				end
+			end
+			default : begin
+				rx_dt_valid = 0;
+				rx_dt = 0;
+			end
+		endcase
+	end
+
+	//Assuming data is valid
+	assign rx_er = 0;
+
+endmodule
